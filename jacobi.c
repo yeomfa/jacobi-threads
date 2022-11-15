be1d9a0f56acc1ce8e1600d95233c71201650352
Omar Fandiño <omarfandinoya@gmail.com>
2022-11-15 01:08:43 -0500

    Checkpoint 3

--- jacobi.c
+++ jacobi.c
@@ -1,14 +1,24 @@
+/* Taller hilos - Jacobi
+ * 
+ * Brian Castaño - 2018214054
+ * Isaac Mejia - 2018114046
+ * Omar Fandiño - 2018114023
+ *
+ * Grupo: 2
+ *
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
-// VARIABLES
-int delta, n, m, t, flag = 0, print = 1;
+// GLOBAL VARIABLES
+int delta, n, m, t;
 float **new_array, **old_array;
-pthread_barrier_t br, br_main;
+pthread_barrier_t br_thread, br_main;
 
 // FUNCTION THREAD
-void * calc ();
+void * thread_calc();
 
 // FUNCTION CALC VALUE
 float calc_val(int, int);
@@ -19,6 +29,7 @@ void print_array(float **);
 // FUNCTION REWRITE
 void rewrite_array();
 
+// FUNCTION MAIN
 int main(int argc, char **argv) {
   int n_threads, thread, *idx;
   pthread_t * handler_threads;
@@ -45,22 +56,22 @@ int main(int argc, char **argv) {
 
   delta = n / n_threads;
 
-  printf("Value n is: %d\n", n);
-  printf("Value m is: %d\n", m);
-  printf("Value n_threads is: %d\n", n_threads);
-  printf("Value t is: %d\n", t);
+  printf("Value rows is: %d\n", n);
+  printf("Value columns is: %d\n", m);
+  printf("Value number threads is: %d\n", n_threads);
+  printf("Value interval is: %d\n", t);
   
   // ASIGNMENT MEMORY SPACE
   handler_threads = malloc(sizeof(pthread_t) * n_threads);
 
-  old_array = malloc(sizeof(float *)*n);
-  for (int i = 0; i < m; i++) {
-    old_array[i] = malloc(sizeof(float)*m);
+  old_array = (float**)malloc(sizeof(float *)*n);
+  for (int i = 0; i < n; i++) {
+    old_array[i] = (float*)malloc(sizeof(float)*m);
   }
 
-  new_array = malloc(sizeof(float *)*n);
-  for (int i = 0; i < m; i++) {
-    new_array[i] = malloc(sizeof(float)*m);
+  new_array = (float**)malloc(sizeof(float *)*n);
+  for (int i = 0; i < n; i++) {
+    new_array[i] = (float*)malloc(sizeof(float)*m);
   }
 
   // READING ARRAY
@@ -75,21 +86,23 @@ int main(int argc, char **argv) {
   printf("Interval initial:\n");
   print_array(old_array);
 
-  pthread_barrier_init(&br, NULL, n_threads+1);
+  pthread_barrier_init(&br_thread, NULL, n_threads+1);
   pthread_barrier_init(&br_main, NULL, n_threads+1);
 
   // CREATE THREADS
   for (thread = 0; thread < n_threads; thread++) {
     idx = malloc(sizeof(int));
     *idx = thread;
-    pthread_create(&handler_threads[thread], NULL, calc, idx); 
+    pthread_create(&handler_threads[thread], NULL, thread_calc, idx); 
   }
 
   // PRINT NEW ARRAY
   for (int i = 0; i<t; i++) {
-    pthread_barrier_wait(&br);
+    printf("Calculating interval [%d]\n", i + 1);
+    pthread_barrier_wait(&br_main);
+    pthread_barrier_wait(&br_thread);
     printf("\n");
-    printf("Interval [%d]:\n", i+1);
+    printf("Result:\n");
     print_array(new_array);
     rewrite_array();
     pthread_barrier_wait(&br_main);
@@ -103,19 +116,21 @@ int main(int argc, char **argv) {
   fclose(file);
   free(old_array);
   free(new_array);
-  pthread_barrier_destroy(&br);
+  pthread_barrier_destroy(&br_thread);
   pthread_barrier_destroy(&br_main);
 
   return EXIT_SUCCESS;
 }
 
-// FUNCTION THREAD
-void * calc( void * id ) {
+// FUNCTION THREAD CALC
+void * thread_calc( void * id ) {
   int id_thread = *((int *) id);
   int to = (id_thread + 1) * delta;
   int from = to - delta;
   float new_value;
   for(int i=0; i<t; i++) {
+    pthread_barrier_wait(&br_main);
+    printf("-> thread %d\n", id_thread+1);
     for (int row = from; row < to; row++) {
       for (int column = 0; column < m; column++) {
         if (row == 0 || column == 0 || row == n-1 || column == m-1) {
@@ -126,7 +141,7 @@ void * calc( void * id ) {
         new_array[row][column] = new_value;
       }
     }
-    pthread_barrier_wait(&br);
+    pthread_barrier_wait(&br_thread);
     pthread_barrier_wait(&br_main);
   }
   free(id);
@@ -151,6 +166,7 @@ void print_array(float **array) {
     }
     printf("\n");
   }
+  printf("\n");
 }
 
 // REWRITE ARRAY
